02c18523d562b64122bc041968bbb24635b2a31d
attilabekecs <attila.bekecs@icloud.com>
2026-02-07 21:46:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { loadCloudState } from "./state/storage.js?v=3";
+import { loadCloudState } from "./state/storage.js?v=2";
 import { initHeader } from "./ui/header.js";
 import { initDialogs } from "./ui/dialogs.js";
 import { initCharacters } from "./features/characters.js";

3b24b527d6633fa17a1ab48eb9597b2706ee9789
attilabekecs <attila.bekecs@icloud.com>
2026-02-07 09:03:15 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 import { initHeader } from "./ui/header.js";
 import { initDialogs } from "./ui/dialogs.js";
 import { initCharacters } from "./features/characters.js";
-import { seedIfEmpty } from "./state/seed.js";
 import { initChaptersCore } from "./chapters/chapters.core.js";
 import { initChapterNotes } from "./chapters/chapters.notes.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  seedIfEmpty();
+document.addEventListener("DOMContentLoaded", async () => {
   initHeader();
   initDialogs();
   initCharacters();

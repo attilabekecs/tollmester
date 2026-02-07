6ea6e47dc38d1b60ecb9086b2a6bc4fb85f7f032
attilabekecs <attila.bekecs@icloud.com>
2026-02-07 09:06:05 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { loadCloudState } from "./state/storage.js";
 import { initHeader } from "./ui/header.js";
 import { initDialogs } from "./ui/dialogs.js";
 import { initCharacters } from "./features/characters.js";
@@ -5,6 +6,8 @@ import { initChaptersCore } from "./chapters/chapters.core.js";
 import { initChapterNotes } from "./chapters/chapters.notes.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+  await loadCloudState();   // ⭐ FELHŐ LETÖLTÉS INDULÁSKOR
+
   initHeader();
   initDialogs();
   initCharacters();

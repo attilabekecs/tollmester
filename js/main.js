d4ecbe648d4901e9285a67e07277c9afb620404b
attilabekecs <attila.bekecs@icloud.com>
2026-02-07 21:48:08 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ import { loadCloudState } from "./state/storage.js?v=3";
 import { initHeader } from "./ui/header.js";
 import { initDialogs } from "./ui/dialogs.js";
 import { initCharacters } from "./features/characters.js?v=2";
-import { initChaptersCore } from "./chapters/chapters.core.js";
-import { initChapterNotes } from "./chapters/chapters.notes.js";
+import { initChaptersCore } from "./chapters/chapters.core.js?v=2";
+import { initChapterNotes } from "./chapters/chapters.notes.js?v=2";
 import { seedIfEmpty } from "./state/seed.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
